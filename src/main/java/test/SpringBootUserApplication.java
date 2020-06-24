8e8aebce69bf0ca493359cf83c070d20beb67e71
chenwanying <haocwy@126.com>
2020-06-24 15:23:59 +0800

    第一次提交

---
+++ src/main/java/test/SpringBootUserApplication.java
@@ -0,0 +1,14 @@
+package test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUserApplication {
+    /**
+     * spring boot的入口
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUserApplication.class,args);
+    }
+}
---
+++ src/main/java/test/controller/startController.java
@@ -0,0 +1,14 @@
+package test.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class startController {
+    @RequestMapping(value="/hello",method= RequestMethod.GET)
+    @ResponseBody
+    public String init(){
+        return "hi,SpringBoot!";
+    }
+}
